3bc6e9a71d57345393d4ebbde44b667a02b4f4f2
Henrik Sommerfeld <henrik@sommerfeld.nu>
2023-12-30 23:14:42 +0100

    Kyria rev3 keymap v0.21

---
+++ keyboards/splitkb/kyria/keymaps/henriksommerfeld/rules.mk
@@ -0,0 +1,4 @@
+OLED_ENABLE = false
+ENCODER_ENABLE = false       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = false     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = false      # Enable keyboard RGB underglow
