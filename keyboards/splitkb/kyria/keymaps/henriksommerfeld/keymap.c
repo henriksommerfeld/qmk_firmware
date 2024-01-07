2b984268bed2adf832723fd462c7b0885abd3c64
Henrik Sommerfeld <henrik@sommerfeld.nu>
2024-01-07 17:35:29 +0100

    Turn off LED on controller

--- keyboards/splitkb/kyria/keymaps/henriksommerfeld/keymap.c
+++ keyboards/splitkb/kyria/keymaps/henriksommerfeld/keymap.c
@@ -203,3 +203,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
+// Turn off Liatris LED
+void keyboard_pre_init_user(void) {
+  // Set our LED pin as output
+  setPinOutput(24);
+  // Turn the LED off
+  // (Due to technical reasons, high is off and low is on)
+  writePinHigh(24);
+}
